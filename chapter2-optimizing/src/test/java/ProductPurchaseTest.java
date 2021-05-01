485b98b8ff0aba0e60ffb1f976fc12ca9ea797de
Carolina Machado <machadoca@google.com>
2021-04-30 19:25:54 -0500

    login & confirm order number scenarios

--- chapter2-optimizing/src/test/java/ProductPurchaseTest.java
+++ chapter2-optimizing/src/test/java/ProductPurchaseTest.java
@@ -14,11 +14,12 @@ public class ProductPurchaseTest {
 
     private WebDriver driver;
     private static String purchaseEmail;
-    private static String puchaseOrderNumber;
+    private static String purchaseOrderNumber;
 
     @BeforeMethod
+    // this  will be started for every test and closed after its completition
     public void testInit() {
-        //todo webdriverManager
+        //to download the correct version of the selected browser's driver
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -29,7 +30,7 @@ public class ProductPurchaseTest {
         driver.quit();
     }
 
-    @Test
+    @Test(priority = 1)
     public void completePurchaseSuuccesfully_whenNewClient() throws InterruptedException {
         driver.navigate().to("http://demos.bellatrix.solutions/");
         //todo *
@@ -56,7 +57,7 @@ public class ProductPurchaseTest {
         quantityBox.sendKeys("2");
         Thread.sleep(5000);
         var updateCar = driver.findElement(By.cssSelector("[name='update_cart']"));
-        var updateCart = driver.findElement(By.cssSelector("[value*='Update cart']"));
+//        var updateCart = driver.findElement(By.cssSelector("[value*='Update cart']"));
         updateCar.click();
         Thread.sleep(4000);
         var totalPrice = driver.findElement(By.cssSelector("[data-title='Total'] " +
@@ -89,11 +90,90 @@ public class ProductPurchaseTest {
         var phone = driver.findElement(By.id("billing_phone"));
         phone.sendKeys("3002114915");
         var email = driver.findElement(By.id("billing_email"));
-        email.sendKeys("dcmachadou@gmail.com");
+        email.sendKeys("info@berlinspaceflowers.com");
+        purchaseEmail = "info@berlinspaceflowers.com";
+        Thread.sleep(5000);
         var placeOrder = driver.findElement(By.id("place_order"));
         placeOrder.click();
         Thread.sleep(5000);
         var orderReceived = driver.findElement(By.cssSelector(".entry-title"));
         Assert.assertEquals(orderReceived.getText(), "Order received");
+    }
+    @Test(priority = 2)
+    public void completePurchaseSuccessfullyWithExistingUser() throws InterruptedException {
+        driver.navigate().to("http://demos.bellatrix.solutions/");
+        var addToCartFalcon9 = driver.findElement(By.cssSelector("[data-product_id*='28']"));
+        addToCartFalcon9.click();
+        Thread.sleep(5000);
+        var viewCart = driver.findElement(By.cssSelector(".added_to_cart"));
+        viewCart.click();
+        var couponCodeTextField = driver.findElement(By.id("coupon_code"));
+        couponCodeTextField.clear();
+        couponCodeTextField.sendKeys("happybirthday");
+        var applyCouponButton = driver.findElement(By.cssSelector("[name='apply_coupon']"));
+        applyCouponButton.click();
+        Thread.sleep(5000);
+        var messageAlert = driver.findElement(By.cssSelector(".woocommerce-message"));
+        Assert.assertEquals(messageAlert.getText(), "Coupon code applied successfully.");
+        var quantityBox = driver.findElement(By.cssSelector(".quantity input"));
+        quantityBox.clear();
+        quantityBox.sendKeys("2");
+        Thread.sleep(5000);
+        var updateCar = driver.findElement(By.cssSelector("[name='update_cart']"));
+        updateCar.click();
+        Thread.sleep(4000);
+        var totalPrice = driver.findElement(By.cssSelector("[data-title='Total'] " +
+                "span.woocommerce-Price-amount"));
+        Assert.assertEquals("114.00€", totalPrice.getText());
+        Thread.sleep(5000);
+        var checkoutCta = driver.findElement(By.cssSelector(".checkout-button"));
+        checkoutCta.click();
+        var showLogin = driver.findElement(By.cssSelector(".showlogin"));
+        showLogin.click();
+        var username = driver.findElement(By.id("username"));
+        username.sendKeys(purchaseEmail);
+        var password = driver.findElement(By.id("password"));
+        password.sendKeys(GetUserPasswordFromDb(purchaseEmail));
+        var loginCta = driver.findElement(By.cssSelector("button[name='login']"));
+        loginCta.click();
+        Thread.sleep(5000);
+        var placeOrder = driver.findElement(By.id("place_order"));
+        placeOrder.click();
+        Thread.sleep(5000);
+        var orderReceived = driver.findElement(By.cssSelector(".entry-title"));
+        Assert.assertEquals(orderReceived.getText(), "Order received");
+        var orderNumber = driver.findElement(By.cssSelector("li.woocommerce-order-overview__order"));
+        purchaseOrderNumber = orderNumber.getText();
+    }
+    @Test(priority = 3)
+    public void correctOrderDataDisplayed_whenNavigateToMyOrderSection() throws InterruptedException {
+        driver.navigate().to("http://demos.bellatrix.solutions/");
+        var myAccountLink = driver.findElement(By.linkText("My account"));
+        myAccountLink.click();
+        var username = driver.findElement(By.id("username"));
+        username.sendKeys(purchaseEmail);
+        var password = driver.findElement(By.id("password"));
+        password.sendKeys(GetUserPasswordFromDb(purchaseEmail));
+        var loginCta = driver.findElement(By.cssSelector("button[name='login']"));
+        loginCta.click();
+        Thread.sleep(5000);
+        var orders = driver.findElement(By.linkText("Orders"));
+        orders.click();
+
+        Thread.sleep(5000);
+        //todo
+        var viewButtons = driver.findElements(By.linkText("View"));
+        viewButtons.get(0).click();
+        Thread.sleep(5000);
+
+        var orderName = driver.findElement(By.xpath("//h1"));
+        String expectedMessage = String.format("ORDER NUMBER #%s", purchaseOrderNumber);
+        Assert.assertEquals(expectedMessage, orderName.getText());
+
+
+    }
+    private String GetUserPasswordFromDb(String userName)
+    {
+        return "@purISQzt%%DYBnLCIhaoG6$";
     }
 }
